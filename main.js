79396b94545b20d5ca2dabbde41e322612fbf394
Alamin <alaminsf40@gmail.com>
2022-02-27 16:47:13 +0600

    firstcommit

--- main.js
+++ main.js
@@ -14,11 +14,17 @@ const read = (text) => {
     speech.text = text;
     if(text == 'hello'){
         speech.text = "yes how can i help you"
-    }else if(text == 'what is your name'){
+    }else if(text == 'what is your name' || text == 'who are you'){
         speech.text = "this is elsa. i am a listener and your assistant"
     }else if(text == "do you love me"){
         speech.text == "no. i do not know you.do not waste your time by using me"
-    }else{
+    }else if(text == 'what is love' ){
+        speech.text = "yes it is just emotion nothing more"
+    }
+    else if(text == 'how many friends do you have' || text =="do you have any friends" || text =="do you have friends"){
+        speech.text = "no. i have no friend. actually i am introvert"
+    }
+    else{
         speech.text = "please try again"
     }
 
